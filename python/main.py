4d9df8e7398313367b4233d990398a2f2b76d171
wiserdivisor <dat0user@gmail.com>
2024-02-24 00:43:42 +0530

    backup

--- python/main.py
+++ python/main.py
@@ -1,19 +1,38 @@
 import os
 import json
 from datetime import date
-from datetime import datetime
 from PyQt6.QtCore import *
 from PyQt6.QtWidgets import *
+
+class TextArea(QTextEdit):
+    def __init__(self,parent):
+        super().__init__(parent=parent)
+        self.show()
+    def keyPressEvent():
 
 class WidgetGallery(QDialog):
 
     def __init__(self, parent=None):
         super(WidgetGallery, self).__init__(parent)
 
-        self.display_window = QVBoxLayout()
-        self.new_label_style = "color:black; background-color: coral; border: 1px solid brown; border-radius: 5px; padding: 10px; font-size: 16px;"
-        self.record_template = { "timestamp":0, "tags":[], "attachments":[], "text":"" }
-        self.todo_template = { "timestamp":0, "checkstate":0, "text":"" }
+        top_layout = QVBoxLayout()
+        mid_layout = QHBoxLayout()
+        btm_layout = QHBoxLayout()
+
+        quote = QLabel()
+        quote.setText("Better to be a smart fella than to be a fart smella")
+        quote.setStyleSheet("font-size:12px; padding:10px;")
+        quote.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        top_layout.addWidget(quote)
+
+        self.log_frame = QVBoxLayout()
+        self.todo_frame = QVBoxLayout()
+        mid_layout.addLayout(self.log_frame)
+        mid_layout.addLayout(self.todo_frame)
+
+        self.log_label_style = "color:black; background-color: coral; border: 1px solid brown; border-radius: 5px; padding: 10px; font-size: 16px;"
+        self.todo_label_style = "color:black; background-color: skyblue; border: 1px solid brown; border-radius: 5px; padding: 10px; font-size: 16px;"
+        self.record_template = { "timestamp":"", "text":""  }
 
         self.text_edit = QTextEdit()
         self.text_edit.setMaximumHeight(100)
@@ -24,18 +43,19 @@ class WidgetGallery(QDialog):
         save_button.setFlat(True)
         save_button.clicked.connect(self.save)
 
-        bottom_layout = QHBoxLayout()
-        bottom_layout.addWidget(self.text_edit)
-        bottom_layout.addWidget(save_button)
+        btm_layout.addWidget(self.text_edit)
+        btm_layout.addWidget(save_button)
 
         main_layout = QGridLayout()
-        main_layout.addLayout(self.display_window,0,0,alignment=Qt.AlignmentFlag.AlignTop)
-        main_layout.addLayout(bottom_layout,1,0,alignment=Qt.AlignmentFlag.AlignBottom)
+        main_layout.addLayout(top_layout,0,0,alignment=Qt.AlignmentFlag.AlignTop)
+        main_layout.addLayout(mid_layout,1,0)
+        main_layout.addLayout(btm_layout,2,0,alignment=Qt.AlignmentFlag.AlignBottom)
         self.setLayout(main_layout)
         self.setWindowTitle("Chitragupt")
-        self.load_display_window()
+        self.load_log_frame()
+        self.resize(640,480)
 
-    def load_display_window(self):
+    def load_log_frame(self):
         current_date = date.today()
         filename = f"./{current_date}.md"
         file_exists = os.path.exists(filename)
@@ -45,20 +65,57 @@ class WidgetGallery(QDialog):
                     newline = newline.strip()
                     new_label = QLabel()
                     new_label.setText(newline)
-                    new_label.setStyleSheet(self.new_label_style)
-                    self.display_window.addWidget(new_label)
+                    new_label.setStyleSheet(self.log_label_style)
+                    self.log_frame.addWidget(new_label)
 
-    def save(self):
-        content = self.text_edit.toPlainText()
+    def open_calendar(self):
+        pass
+
+    def save_todo(self,content):
+        new_label = QLabel()
+        new_label.setText(content)
+        new_label.setStyleSheet(self.todo_label_style)
+        self.todo_frame.addWidget(new_label)
+    
+    def save_log(self,content):
         current_date = date.today()
-        filename = f"{current_date}.md"
-        content = self.text_edit.toPlainText()
+        filename = f"./{current_date}.md"
         with open(filename,'a') as fptr:
             fptr.write(content + "\n")
         new_label = QLabel()
         new_label.setText(content)
-        new_label.setStyleSheet(self.new_label_style)
-        self.display_window.addWidget(new_label)
+        new_label.setStyleSheet(self.log_label_style)
+        self.log_frame.addWidget(new_label)
+
+
+    def goto_date(self,date):
+        pass
+
+    def set_reminder(self,timestamp,message):
+        pass
+    
+    def save(self):
+        content = self.text_edit.toPlainText()
+        content = content.strip()
+        if(content[0]==":"):
+            print(content)
+            content_list = content.split("\n")
+            command_args = content_list[0].split(" ")
+            match command_args[0]:
+                case ":cal":
+                    self.open_calendar()
+                case ":todo":
+                    content_list[1]
+                    self.save_todo(content_list[1])
+                case ":goto":
+                    self.goto_date(command_args[1])
+                case ":remind":
+                    self.set_reminder(timestamp=command_args[1],message=content_list[1])
+                case _:
+                    print(f"Unknown Command: {command_args[0]}")
+        else:
+            self.save_log(content)
+
         self.text_edit.setPlainText("")
 
 if __name__ == "__main__":
